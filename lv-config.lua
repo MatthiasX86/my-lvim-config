8663c2ad0d0b2c2ef6f1825c30d0f408ee72ce2a
MatthiasX86 <28681206+MatthiasX86@users.noreply.github.com>
2021-07-31 19:54:56 -0400

    comment strings settings

--- lv-config.lua
+++ lv-config.lua
@@ -346,7 +346,7 @@ lvim.plugins = {
           vim.api.nvim_command("autocmd InsertEnter * let b:cursorword = 0")
           vim.api.nvim_command("autocmd InsertLeave * let b:cursorword = 1")
           vim.api.nvim_command("augroup END")
-          end
+        end
     },
 
     {
@@ -390,6 +390,96 @@ lvim.plugins = {
 
     {
       "fladson/vim-kitty"
+    },
+
+    {
+      "s1n7ax/nvim-comment-frame",
+      requires = {
+          { 'nvim-treesitter' }
+      },
+      config = function()
+        require('nvim-comment-frame').setup({
+          keymap = '<Bslash>c',
+          multiline_keymap = '<Bslash>C',
+          languages = {
+            -- configuration for Lua programming language
+            -- @NOTE global configuration will be overridden by language level
+            -- configuration if provided
+            lua = {
+                -- start the comment with this string
+                start_str = '--[[',
+                -- end the comment line with this string
+                end_str = ']]--',
+                -- fill the comment frame border with this character
+                fill_char = '*',
+                -- width of the comment frame
+                frame_width = 100,
+                -- wrap the line after 'n' characters
+                line_wrap_len = 70,
+                -- automatically indent the comment frame based on the line
+                auto_indent = false,
+                -- add comment above the current line
+                add_comment_above = false,
+            },
+          }
+        })
+      end
+    },
+
+    {
+      "folke/zen-mode.nvim",
+      config = function()
+        require("zen-mode").setup {
+          window = {
+            backdrop = 0.95, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+            -- height and width can be:
+            -- * an absolute number of cells when > 1
+            -- * a percentage of the width / height of the editor when <= 1
+            -- * a function that returns the width or the height
+            width = 120, -- width of the Zen window
+            height = 1, -- height of the Zen window
+            -- by default, no options are changed for the Zen window
+            -- uncomment any of the options below, or add other vim.wo options you want to apply
+            options = {
+              -- signcolumn = "no", -- disable signcolumn
+              -- number = false, -- disable number column
+              -- relativenumber = false, -- disable relative numbers
+              -- cursorline = false, -- disable cursorline
+              -- cursorcolumn = false, -- disable cursor column
+              -- foldcolumn = "0", -- disable fold column
+              -- list = false, -- disable whitespace characters
+            },
+          },
+          plugins = {
+            -- disable some global vim options (vim.o...)
+            -- comment the lines to not apply the options
+            options = {
+              enabled = true,
+              ruler = false, -- disables the ruler text in the cmd line area
+              showcmd = false, -- disables the command in the last line of the screen
+            },
+            twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+            gitsigns = { enabled = false }, -- disables git signs
+            tmux = { enabled = false }, -- disables the tmux statusline
+            -- this will change the font size on kitty when in zen mode
+            -- to make this work, you need to set the following kitty options:
+            -- - allow_remote_control socket-only
+            -- - listen_on unix:/tmp/kitty
+            kitty = {
+              enabled = true,
+              font = "+4", -- font size increment
+            },
+          },
+          -- callback where you can add custom code when the Zen window opens
+          on_open = function(win)
+          end,
+          -- callback where you can add custom code when the Zen window closes
+          on_close = function()
+          end,
+        }
+
+        vim.api.nvim_set_keymap("n", "<Bslash>z", ":ZenMode<Enter>", { noremap = true, silent = true })
+      end
     }
 
 }
