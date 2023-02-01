cc869353616fd7397f6c442dba52b9960e947177
Matthew <28681206+MatthiasX86@users.noreply.github.com>
2023-01-31 21:19:51 -0500

    new plugins

--- config.lua
+++ config.lua
@@ -213,10 +213,10 @@ lvim.plugins = {
             },
           },
           coc_nvim = false,
-          lsp_trouble = false,
+          lsp_trouble = true,
           cmp = true,
           lsp_saga = false,
-          gitgutter = false,
+          gitgutter = true,
           gitsigns = true,
           leap = false,
           telescope = true,
@@ -237,18 +237,18 @@ lvim.plugins = {
           which_key = false,
           indent_blankline = {
             enabled = true,
-            colored_indent_levels = false,
+            colored_indent_levels = true,
           },
           dashboard = true,
           neogit = false,
-          vim_sneak = false,
+          vim_sneak = true,
           fern = false,
           barbar = false,
           bufferline = true,
           markdown = true,
           lightspeed = false,
-          ts_rainbow = false,
-          hop = false,
+          ts_rainbow = true,
+          hop = true,
           notify = true,
           telekasten = true,
           symbols_outline = true,
@@ -310,13 +310,13 @@ lvim.plugins = {
     end
   },
 
-  {
-    "ray-x/lsp_signature.nvim",
-    config = function()
-      require "lsp_signature".on_attach()
-    end,
-    event = { "InsertEnter" }
-  },
+  -- {
+  --   "ray-x/lsp_signature.nvim",
+  --   config = function()
+  --     require "lsp_signature".on_attach()
+  --   end,
+  --   event = { "InsertEnter" }
+  -- },
 
   {
     "phaazon/hop.nvim",
@@ -574,7 +574,7 @@ lvim.plugins = {
     cmd = "SymbolsOutline",
     config = function()
       require("symbols-outline").setup {}
-      vim.api.nvim_set_keymap("n", "<Bslash>o", ":SymbolsOutline<Enter>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<Bslash>os", ":SymbolsOutline<Enter>", { noremap = true, silent = true })
     end
   },
 
@@ -847,6 +847,10 @@ lvim.plugins = {
   {
     "michaelb/sniprun",
     run = 'bash ./install.sh',
+    config = function()
+      vim.api.nvim_set_keymap("v", "<Bslash>sr", ":'<,'>SnipRun<Enter>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<Bslash>ssc", ":SnipClose<Enter>", { noremap = true, silent = true })
+    end
   },
 
   {
@@ -1270,7 +1274,253 @@ lvim.plugins = {
 
   { 'tikhomirov/vim-glsl' },
 
-  { 'timtro/glslView-nvim', ft = 'glsl' }
+  { 'timtro/glslView-nvim', ft = 'glsl' },
+
+  {
+    "folke/noice.nvim",
+    config = function()
+      require("noice").setup({
+        lsp = {
+          signature = {
+            enable = true
+          }
+        }
+      })
+    end,
+    requires = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+      "nvim-treesitter/nvim-treesitter"
+    }
+  },
+
+  {
+    "stevearc/overseer.nvim",
+    config = function()
+      require('overseer').setup()
+      vim.api.nvim_set_keymap("n", "<Bslash>oor", ":OverseerRunCmd ", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<Bslash>oot", ":OverseerToggle<Enter>", { noremap = true, silent = true })
+    end
+  },
+
+  {
+    'simrat39/rust-tools.nvim',
+    config = function()
+      local opts = {
+        tools = { -- rust-tools options
+
+          -- how to execute terminal commands
+          -- options right now: termopen / quickfix
+          executor = require("rust-tools.executors").termopen,
+
+          -- callback to execute once rust-analyzer is done initializing the workspace
+          -- The callback receives one parameter indicating the `health` of the server: "ok" | "warning" | "error"
+          on_initialized = nil,
+
+          -- automatically call RustReloadWorkspace when writing to a Cargo.toml file.
+          reload_workspace_from_cargo_toml = true,
+
+          -- These apply to the default RustSetInlayHints command
+          inlay_hints = {
+            -- automatically set inlay hints (type hints)
+            -- default: true
+            auto = true,
+
+            -- Only show inlay hints for the current line
+            only_current_line = false,
+
+            -- whether to show parameter hints with the inlay hints or not
+            -- default: true
+            show_parameter_hints = true,
+
+            -- prefix for parameter hints
+            -- default: "<-"
+            parameter_hints_prefix = "<- ",
+
+            -- prefix for all the other hints (type, chaining)
+            -- default: "=>"
+            other_hints_prefix = "=> ",
+
+            -- whether to align to the length of the longest line in the file
+            max_len_align = false,
+
+            -- padding from the left if max_len_align is true
+            max_len_align_padding = 1,
+
+            -- whether to align to the extreme right or not
+            right_align = false,
+
+            -- padding from the right if right_align is true
+            right_align_padding = 7,
+
+            -- The color of the hints
+            highlight = "Comment",
+          },
+
+          -- options same as lsp hover / vim.lsp.util.open_floating_preview()
+          hover_actions = {
+
+            -- the border that is used for the hover window
+            -- see vim.api.nvim_open_win()
+            border = {
+              { "╭", "FloatBorder" },
+              { "─", "FloatBorder" },
+              { "╮", "FloatBorder" },
+              { "│", "FloatBorder" },
+              { "╯", "FloatBorder" },
+              { "─", "FloatBorder" },
+              { "╰", "FloatBorder" },
+              { "│", "FloatBorder" },
+            },
+
+            -- Maximal width of the hover window. Nil means no max.
+            max_width = nil,
+
+            -- Maximal height of the hover window. Nil means no max.
+            max_height = nil,
+
+            -- whether the hover action window gets automatically focused
+            -- default: false
+            auto_focus = false,
+          },
+
+          -- settings for showing the crate graph based on graphviz and the dot
+          -- command
+          crate_graph = {
+            -- Backend used for displaying the graph
+            -- see: https://graphviz.org/docs/outputs/
+            -- default: x11
+            backend = "x11",
+            -- where to store the output, nil for no output stored (relative
+            -- path from pwd)
+            -- default: nil
+            output = nil,
+            -- true for all crates.io and external crates, false only the local
+            -- crates
+            -- default: true
+            full = true,
+
+            -- List of backends found on: https://graphviz.org/docs/outputs/
+            -- Is used for input validation and autocompletion
+            -- Last updated: 2021-08-26
+            enabled_graphviz_backends = {
+              "bmp",
+              "cgimage",
+              "canon",
+              "dot",
+              "gv",
+              "xdot",
+              "xdot1.2",
+              "xdot1.4",
+              "eps",
+              "exr",
+              "fig",
+              "gd",
+              "gd2",
+              "gif",
+              "gtk",
+              "ico",
+              "cmap",
+              "ismap",
+              "imap",
+              "cmapx",
+              "imap_np",
+              "cmapx_np",
+              "jpg",
+              "jpeg",
+              "jpe",
+              "jp2",
+              "json",
+              "json0",
+              "dot_json",
+              "xdot_json",
+              "pdf",
+              "pic",
+              "pct",
+              "pict",
+              "plain",
+              "plain-ext",
+              "png",
+              "pov",
+              "ps",
+              "ps2",
+              "psd",
+              "sgi",
+              "svg",
+              "svgz",
+              "tga",
+              "tiff",
+              "tif",
+              "tk",
+              "vml",
+              "vmlz",
+              "wbmp",
+              "webp",
+              "xlib",
+              "x11",
+            },
+          },
+        },
+
+        -- all the opts to send to nvim-lspconfig
+        -- these override the defaults set by rust-tools.nvim
+        -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
+        server = {
+          -- standalone file support
+          -- setting it to false may improve startup time
+          standalone = true,
+        }, -- rust-analyzer options
+
+        -- debugging stuff
+        dap = {
+          adapter = {
+            type = "executable",
+            command = "lldb-vscode",
+            name = "rt_lldb",
+          },
+        },
+      }
+
+      require('rust-tools').setup(opts)
+    end,
+    requires = {
+      "neovim/nvim-lspconfig",
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap'
+    }
+  },
+
+  {
+    "lewis6991/hover.nvim",
+    config = function()
+      require("hover").setup {
+        init = function()
+          -- Require providers
+          require("hover.providers.lsp")
+          require('hover.providers.gh')
+          require('hover.providers.gh_user')
+          -- require('hover.providers.jira')
+          require('hover.providers.man')
+          require('hover.providers.dictionary')
+        end,
+        preview_opts = {
+          border = nil
+        },
+        -- Whether the contents of a currently open hover window should be moved
+        -- to a :h preview-window when pressing the hover keymap.
+        preview_window = true,
+        title = true
+      }
+
+      -- Setup keymaps
+      vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+      vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+    end
+  }
 
 }
 
@@ -1280,6 +1530,48 @@ lvim.plugins = {
 require 'lspconfig'.tailwindcss.setup {}
 require 'lspconfig'.cssmodules_ls.setup {}
 require 'lspconfig'.solang.setup {}
+
+-- Debug Adapter Protocol: DAP
+
+-- local dap = require("dap")
+
+-- dap.adapters.codelldb = {
+--   type = 'server',
+--   port = "${port}",
+--   executable = {
+--     command = '/home/matthew/.local/share/nvim/mason/bin/codelldb',
+--     args = { "--port", "${port}" },
+--   }
+-- }
+
+-- dap.configurations.cpp = {
+--   {
+--     name = "Launch file",
+--     type = "codelldb",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input('/home/matthew/.local/share/nvim/mason/bin/codelldb', vim.fn.getcwd() .. '/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}',
+--     stopOnEntry = false,
+--   },
+-- }
+
+-- dap.configurations.rust = dap.configurations.cpp
+
+
+-- require('dap').configurations.rust = {
+--   {
+--     name = "Launch file",
+--     type = "codelldb",
+--     request = "launch",
+--     program = function()
+--       return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+--     end,
+--     cwd = '${workspaceFolder}',
+--     stopOnEntry = false,
+--   },
+-- }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 
